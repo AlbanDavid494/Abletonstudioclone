f3b70cd975e208dc1f38921a7ab2f91cad572127
Alban David <albandavid494@gmail.com>
2023-06-06 17:26:16 -0700

    files commited

---
+++ login.js
@@ -0,0 +1,7 @@
+// const moreLink = document.getElementsByClassName('nav-links-1');
+moreItems.style.fontSize = '5rem';
+// const horizontalLine = document.getElementsByClassName('span');
+// const verticalLine = document.getElementsByClassName('span-1');
+const moreItems = document.getElementsByClassName('more-items');
+// const moreItem = document.getElementsByClassName('More-items');
+// console.log(moreLink);
---
+++ trial.js
@@ -0,0 +1,3 @@
+document.addEventListener('click', function() {
+    const plus
+})
